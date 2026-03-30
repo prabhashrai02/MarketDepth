50c2e6f467685b01e81b930c59aa3ba0b845ea17
Prabhash Rai <prabhashsahaj@gmail.com>
2026-03-30 22:48:02 +0530

    chore: update dependencies and build configuration

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -4,4 +4,17 @@ import { pluginReact } from '@rsbuild/plugin-react';
 // Docs: https://rsbuild.rs/config/
 export default defineConfig({
   plugins: [pluginReact()],
+  resolve: {
+    alias: {
+      '@': './src',
+      '@/components': './src/components',
+      '@/features': './src/features',
+      '@/services': './src/services',
+      '@/types': './src/types',
+      '@/utils': './src/utils',
+      '@/constants': './src/constants',
+      '@/contexts': './src/contexts',
+      '@/styles': './src/styles',
+    },
+  },
 });
